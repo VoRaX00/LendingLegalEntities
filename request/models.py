4059a5529e672ad18c352e15e52f0366dc1c209c
nikita <nikitakerzhik@gmail.com>
2024-12-28 22:37:21 +1000

    update model Request and init migrations

--- request/models.py
+++ request/models.py
@@ -6,8 +6,14 @@ from legal_user.models import LegalUser
 
 
 class Request(models.Model):
+    choices = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    ]
+
     id = models.AutoField(primary_key=True)
     inn_legal_user = models.ForeignKey(LegalUser, on_delete=models.CASCADE)
     credit_product_id = models.ForeignKey(CreditProduct, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10, choices=('одобрено', 'обрабатывается', 'отклонено'))
+    status = models.CharField(max_length=10, choices=choices, default='pending')
     email_admin = models.ForeignKey(Administrator, on_delete=models.CASCADE)
