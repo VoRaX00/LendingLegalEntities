0673d76affad691c3af0a0e454981405573629c3
nikita <nikitakerzhik@gmail.com>
2024-12-28 19:22:13 +1000

    init legal_user

---
+++ lending/legal_user/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class LegalUser(models.Model):
+    inn = models.IntegerField(unique=True, primary_key=True)
+    name = models.CharField(unique=True, max_length=50)
+    type_activity = models.CharField(unique=True, max_length=50)
+    contact_person = models.CharField()
+    address = models.CharField()
